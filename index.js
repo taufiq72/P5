c812c7feb8e99e038fe49d37990ce8f9d799f365
taufiq72 <taufiqq1207@gmail.com>
2026-07-20 10:35:50 +0700

    menambahkan db.sequelize.sync pada index.js

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
     console.log('Server started on port 3000');
 });
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });

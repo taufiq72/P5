cfd929fced97fb874701fa2e3a7ad1f56a6d4632
taufiq72 <taufiqq1207@gmail.com>
2026-07-20 10:34:53 +0700

    menambahkan app.use pada index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const db = require('./models');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
